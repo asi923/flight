c659e364778ba3da01a7a1a93a4a3ededae31afd
asi923 <143652000+asi923@users.noreply.github.com>
2024-06-29 19:58:05 +0300

    Add files via upload

---
+++ public/src/assets/login/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiUserPlus } from 'react-icons/fi';
+
+import './styles.css';
+
+import api from '../../services/api';
+
+import logo from '../../assets/big-logo.svg';
+import plane from '../../assets/big-plane.svg'
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try{
+            const response = await api.post('sessions', { email, password });
+
+            localStorage.setItem('airlineEmail', email);
+            localStorage.setItem('airlinePassword', password);
+            localStorage.setItem('airlineName', response.data.name);
+
+            history.push('/profile');
+        } catch(err) {
+            alert('Login failed, please try again.');
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logo} alt="FGAirlines" />
+                
+                <form onSubmit={handleLogin}>
+                    <h1>Log in</h1>
+
+                    <input placeholder="Your E-mail" 
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                        style={{marginBottom: "10px"}}
+                    />
+                    <input placeholder="Your Password" 
+                        type="password"
+                        value={password}
+                        onChange={e=>setPassword(e.target.value)}
+                        style={{marginBottom: "5px"}}
+                    />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiUserPlus size={16} color="#17333C" />
+                        Don't have a registration? Register
+                    </Link>
+                </form>
+            </section>
+
+            <img className="Big-plane" src={plane} alt="Airplane" />
+        </div>
+    );}
---
+++ public/src/assets/login/newFlight/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg'
+
+export default function NewFlight() {
+    const [destiny, setDestiny] = useState('');
+    const [data, setData] = useState('');
+    const [hour, setHour] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const airlineId = localStorage.getItem('airlineId');
+
+    async function handleNewFlight(e) {
+        e.preventDefault();
+
+        const dataFlight = {
+            destiny,
+            data,
+            hour,
+            value,
+        };
+
+        try {
+            await api.post('flights', dataFlight, {
+                headers: {
+                    Authorization: airlineId,
+                }
+            })
+
+            history.push('/profile');
+        } catch(err) {
+            alert('Error creating new flight, please try again.');
+        }
+    }
+
+    return (
+        <div className="new-flight-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="FGAirlines" />
+
+                    <h1>Register new flight</h1>
+                    <p>Register a new flight and put it up for sale right now!</p>
+
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#17333C" />
+                        Voltar
+                    </Link>
+                </section>
+
+                <form onSubmit={handleNewFlight}>
+                    <input 
+                        placeholder="Fate" 
+                        value={destiny}
+                        onChange={e => setDestiny(e.target.value)}
+                    />
+                    <div className="form-group">
+                        <input 
+                            placeholder="Data" 
+                            value={data}
+                            onChange={e => setData(e.target.value)}
+                        />
+                        <input 
+                            placeholder="Hour" 
+                            value={hour}
+                            onChange={e => setHour(e.target.value)}
+                        />
+                    </div>
+                    <input 
+                        placeholder="Value (in reais)" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button className="button" type="submit">Register</button>
+                    
+                        
+                </form>
+            </div>
+        </div>
+    );
+}
